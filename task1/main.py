c9423ab7dfa513a40721e3fcdbea2bdb8201d582
niduolaoa <2455512051@qq.com>
2023-09-03 21:56:45 +0800

    任务一代码跑通

---
+++ task1/main.py
@@ -0,0 +1,30 @@
+import numpy
+import csv
+import random
+from feature import Bag,Gram
+from compassion import alpha_gradient_plot
+ 
+# 数据读取
+data_file_path = r"E:\Graduate\NLP\datasets\sentiment-analysis-on-movie-reviews\train.tsv"
+with open(data_file_path) as f:
+    tsvreader = csv.reader(f, delimiter='\t')
+    temp = list(tsvreader)
+ 
+# 初始化
+data = temp[1:]
+max_item=1000
+random.seed(2023)
+numpy.random.seed(2023)
+ 
+# 特征提取
+bag=Bag(data,max_item)
+bag.get_words()
+bag.get_matrix()
+ 
+gram=Gram(data, dimension=2, max_item=max_item)
+gram.get_words()
+gram.get_matrix()
+ 
+# 画图
+alpha_gradient_plot(bag,gram,10000,10)  # 计算10000次
+alpha_gradient_plot(bag,gram,100000,10)  # 计算100000次
